e1e9210df08ca30e67f7775517915e4e83827689
Andrés Ibarra Guerrero <andresmcio@Andress-MacBook-Pro.local>
2023-05-22 19:23:10 -0500

    Pagination working

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -5,6 +5,17 @@ module.exports = {
     getAll: (req, res) => {
         let data = readFileSync(resolve(__dirname, '../database/data/employees.json'));
         const employees = JSON.parse(data);
+        let page = parseInt(req.query.page);
+
+        if(page) {
+            const limit = 2;
+            let start = (page - 1) * limit;
+            let end = ((page - 1) * limit) + 1;
+            let result = employees.slice(start, end);
+
+            return res.json(result);
+        }
+
         return res.json(employees);
     }
 };
